e4aeb71d348afffebf401c14ba61745a49c6d840
Manuel Chavarria <manuelchavarria528@gmail.com>
2024-03-06 01:11:36 -0600

    creacion de endpoints de usuarios y playlist

---
+++ youtkids/src/App.js
@@ -0,0 +1,12 @@
+import CreateUser from './users/createUser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateUser></CreateUser>
+    </div>
+  );
+}
+
+export default App;
